351c26cdca613928fec8bb476ccc15c604a9d3ad
DevasenaInupakutika <devasena.prasad@gmail.com>
2014-11-19 13:10:02 +0000

    Initial Commit

---
+++ CV_Project1/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  CV_Project1
+//
+//  Created by Devasena on 19/11/2014.
+//  Copyright (c) 2014 Devasena. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
---
+++ CV_Project1Tests/CV_Project1Tests.m
@@ -0,0 +1,34 @@
+//
+//  CV_Project1Tests.m
+//  CV_Project1Tests
+//
+//  Created by Devasena on 19/11/2014.
+//  Copyright (c) 2014 Devasena. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface CV_Project1Tests : XCTestCase
+
+@end
+
+@implementation CV_Project1Tests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
