bb152719b51d29ce9fb7da34d1ad5fccbd9b1ca0
DevasenaInupakutika <devasena.prasad@gmail.com>
2014-11-20 16:03:06 +0000

    First opencv program in C after setting up OpenCV in MAC

---
+++ CV_Project1/Document.h
@@ -0,0 +1,13 @@
+//
+//  Document.h
+//  CV_Project1
+//
+//  Created by Devasena on 19/11/2014.
+//
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface Document : NSPersistentDocument
+
+@end
---
+++ CV_Project1/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  CV_Project1
+//
+//  Created by Devasena on 19/11/2014.
+//
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[])
+{
+    return NSApplicationMain(argc, argv);
+}
